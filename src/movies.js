43ab6f4786eefedcb1de065b771e598976b84261
Juan Pablo Castano <jpcastanom@gmail.com>
2021-06-25 09:37:03 -0500

    iterations 1,2,3,4

--- src/movies.js
+++ src/movies.js
@@ -1,22 +1,55 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(arr) {
+  return arr.map((movie) => {
+    return movie.director
+  })
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(arr) {
+  return arr.filter((movie) => {
+    return movie.director === "Steven Spielberg"
+  }).filter((movie) => {
+    return movie.genre.includes('Drama')
+  }).length
+}
+
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(arr) {
+  return arr.reduce((avg, movie)=>{
+    if(movie.score === undefined){
+      return avg
+    } else {
+      return avg + Number((movie.score/arr.length).toFixed(2))
+    }
+  },0)
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movies) {
+  let dramaMovies = movies.filter((movie)=>{
+    return movie.genre.includes("Drama");
+  })
+
+  return dramaMovies.reduce((avg, movie)=>{
+    if (movie.score === undefined){
+      return avg
+    } else {
+      return avg + Number((movie.score/dramaMovies.length).toFixed(2))
+    }
+  },0)
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(movies) {
+  
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically(movies) {}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
